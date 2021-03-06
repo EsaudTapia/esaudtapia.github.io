dd6ab0c278c903b17907abcff29a754632e1fe1e
MR.Tapia-III <67276642+EsaudTapia@users.noreply.github.com>
2021-03-06 14:37:57 -0600

    Add files via upload

---
+++ js/alertas.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function proximamente(){
+    
+Swal.fire({
+  title: 'No esta lista!',
+  text: 'Proximamente',
+  imageUrl: 'https://images.wikidexcdn.net/mwuploads/esssbwiki/thumb/0/09/latest/20140911193903/Sprite_del_Perro_de_Duck_Hunt.gif/180px-Sprite_del_Perro_de_Duck_Hunt.gif',
+  imageWidth: 200,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+})
+
+}
